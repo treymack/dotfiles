d3999543645f904c27e3c97cdd664bcf3a35ed32
Trey Mack <tmack10@gmail.com>
2021-04-18 12:26:03 -0400

    powershell - ctrl+d exits shell

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,8 @@
+# Work around an RS5/PSReadline-2.0.0+beta2 bug (Spacebar is not marked 'essential')
+Set-PSReadlineKeyHandler "Shift+SpaceBar" -ScriptBlock {
+    [Microsoft.Powershell.PSConsoleReadLine]::Insert(' ')
+}
+
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
 
 function which($cmd) {
