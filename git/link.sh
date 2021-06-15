a87dd84f5fe572309d07ff92d6366e0943ab8a01
Trey Mack <tmack10@gmail.com>
2021-06-15 11:25:42 -0400

    MacOS changes

---
+++ git/link.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf "$HOME/dotfiles/git/.gitconfig" "$HOME"
+ln -sf "$HOME/dotfiles/git/.gitignore" "$HOME"
---
+++ vscode/link.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -sf "$HOME/dotfiles/vscode/keybindings.json" "$HOME/Library/Application Support/Code/User"
+ln -sf "$HOME/dotfiles/vscode/settings.json" "$HOME/Library/Application Support/Code/User"
