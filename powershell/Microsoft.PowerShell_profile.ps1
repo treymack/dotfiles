a8873682ecfb382bdecd7afea8fe8dad02b3776e
Trey Mack <tmack10@gmail.com>
2023-02-02 19:17:22 -0500

    who knows, it's been a couple years

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -9,10 +9,38 @@ Set-PSReadlineKeyHandler -Key ctrl+l -Function ClearScreen
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-PoshPrompt -Theme trey
+function Set-EnvVar {
+    $env:POSH_AZ_SUBSCRIPTION_NAME = Get-AzContext | Select-Object -ExpandProperty "Subscription" | Select-Object -ExpandProperty "Name"
+}
+New-Alias -Name 'Set-PoshContext' -Value 'Set-EnvVar' -Scope Global -Force
+
+# rancher + nerdctl
+Set-Alias -Name docker -Value nerdctl
 
 # k8s
 Set-Alias k kubectl
-Set-Alias mk minikube
+# Set-Alias mk minikube
+# try {
+# following line is slow at powershell startup, commenting unless using
+# docker + minikube
+
+# & minikube -p minikube docker-env --shell powershell | Invoke-Expression
+# }
+# catch {
+# Write-Error "Cannot connect docker.exe to minikube's dockerd"
+# }
+#
+# if (Get-Command kubectl -ErrorAction SilentlyContinue) {
+#     kubectl completion powershell | Out-String | Invoke-Expression
+# }
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
 
 function which($cmd) {
     Get-Command $cmd | Select-Object -ExpandProperty path
@@ -21,19 +49,16 @@ function which($cmd) {
 function shrug { Write-Output '¯\_(ツ)_/¯'; }
 function fliptable { Write-Output '（╯°□°）╯ ┻━┻'; } # Flip a table. Example usage: fsck -y /dev/sdb1 || fliptable
 
-function touch
-{
+function touch {
     $file = $args[0]
-    if($file -eq $null) {
+    if ($file -eq $null) {
         throw "No filename supplied"
     }
 
-    if(Test-Path $file)
-    {
+    if (Test-Path $file) {
         (Get-ChildItem $file).LastWriteTime = Get-Date
     }
-    else
-    {
+    else {
         echo $null > $file
     }
 }
